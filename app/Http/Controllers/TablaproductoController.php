86c21c9634ffee81e778d84d9345085f200f724d
guidoadrian97 <guidoadrian97@hotmail.com>
2020-02-06 00:53:36 -0500

    CRUDPRODUCTO

--- app/Http/Controllers/TablaproductoController.php
+++ app/Http/Controllers/TablaproductoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Producto;
-
+use App\CategoriaProducto;
 
 class TablaproductoController extends Controller
 {
@@ -15,8 +15,9 @@ class TablaproductoController extends Controller
      */
     public function index()
     {
+        $categoria=CategoriaProducto::all();
         $producto=Producto::orderBy('id','asc')->where('eliminadolog','1')->paginate(3);
-        return view('adminlte::Paginas.TablaProductos',compact('producto'));
+        return view('adminlte::Paginas.TablaProductos',compact('producto','categoria'));
     }
 
    /**
@@ -36,6 +37,33 @@ class TablaproductoController extends Controller
      */
     public function store(Request $request)
     {
+        $Tipo=CategoriaProducto::find($request->Cate1);
+       $file=$request->file('imagen2');
+       $name=$file->getClientMimeType();
+       $tipoimg=str_replace('image/','.',$name);
+       $TipoImagen=uniqid();
+       $FileName=$Tipo->id.'/'.$TipoImagen.$tipoimg;
+       $path=public_path().'/imagen/'.$Tipo->id;
+       $file->move($path,$FileName); 
+$tipo=Producto::find($request->id2);
+$aux=$tipo->imagenProducto;
+       unlink(public_path().'/imagen/'.$aux);
+       
+        
+        $tipo->codigoProducto=$request->codigo;
+        $tipo->nombreProducto=$request->nombre;
+        $tipo->descipcionProducto=$request->descripcion;
+        $tipo->marcaProducto=$request->marca;
+        $tipo->modeloProducto=$request->modelo;
+        
+        $tipo->imagenProducto=$FileName;  
+        $tipo->fecha_caducidadProducto=$request->fecha;
+        $tipo->categoriaproducto_id=$request->Cate1;
+        
+
+        
+        $tipo->save();
+        return redirect()->action('ProductoController@index');
     }
 
     /**
@@ -57,7 +85,9 @@ class TablaproductoController extends Controller
      */
     public function edit($id)
     {
-        //
+        $categoria=CategoriaProducto::all();
+        $ProducEdit=Producto::find($id);
+        return view('adminlte::Paginas.EditarProducto', compact('ProducEdit','categoria'));
     }
 
     /**
@@ -69,7 +99,8 @@ class TablaproductoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       
+       
     }
 
     /**
